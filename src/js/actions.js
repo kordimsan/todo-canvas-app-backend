cf32b55458a666f1fd3185075e8d3abbab0e9202
Korzunin Dmitry <dakorzunin@sberbank.ru>
2021-10-13 12:00:06 +0500

    fix

--- src/js/actions.js
+++ src/js/actions.js
@@ -1,28 +1,44 @@
 // "udovolstvie" - id эмоции
 function addNote(note, context) {
-    addAction({
-        type: "add_note",
-        note: note,
-    }, context, "udovolstvie");
+  addAction(
+    {
+      type: "add_note",
+      note: note,
+    },
+    context,
+    "udovolstvie"
+  );
 }
 
 function getTable(table, context) {
-    addAction({
-        type: "get_table",
-        table: table,
-    }, context, "udovolstvie");
+  addAction(
+    {
+      type: "get_table",
+      note: table,
+    },
+    context,
+    "udovolstvie"
+  );
 }
 
-function doneNote(id, context){
-    addAction({
-        type: "done_note",
-        id: id
-    }, context, "udovolstvie");
+function doneNote(id, context) {
+  addAction(
+    {
+      type: "done_note",
+      id: id,
+    },
+    context,
+    "udovolstvie"
+  );
 }
 
-function deleteNote(id, context){
-    addAction({
-        type: "delete_note",
-        id: id 
-    }, context, "udovolstvie");
+function deleteNote(id, context) {
+  addAction(
+    {
+      type: "delete_note",
+      id: id,
+    },
+    context,
+    "udovolstvie"
+  );
 }
